dc52327e802e46e36be8e942784b283f938d1c50
slowest <soslow4u@gmail.com>
2022-12-03 18:11:49 +0900

    first 3 days

---
+++ task 3.js
@@ -0,0 +1,424 @@
+const rucksacks = `shzsFcPssFhjFssBzdpRcNHNZrpdJdJVJZ
+fwvMCntfCCbSbSbtDgDNrDtDtJHZVH
+GbCwwbwwnGrLhBzjFFFsWPhL
+PpCqRsqqmmtCwMJC
+LHFrLLHDSNHlfWNhDzmjzzJlJzPJMvPJjQ
+SGSWDNrhZhPDSWDZLgVVRgbRppgpGVnpnn
+GRRjbVjmJZlgMRzzrN
+FpDptHpfHfnpPTvDFTWpFPnPcMfNCClNrzcVcrMMzVsCZlsZ
+TFTQDnvLHPFDtVbLwbjdGjdwwJ
+lhljvvhCjjzhjszzBPmnmGVZMGzG
+FbTcTwbtSFdtcMPnTBPQVnnBZT
+SFMpHDtNDSSbSdwppvgJWjJCJJgWgvlJHH
+wzNCWpzCzJnWWpRRNdJrgHLhjfbLrHrchV
+lBMStmPmmLQDPQZlshrdhgrfrcrrddgHgs
+mvGDGQSvDPBlGMLGCvCWpNvpzRWFwqRw
+stBttBThtDZqPWssPWZp
+gRggwwggCGFSBBvPRpHZZrHdZLZq
+ccFJGCNJmmGQzbTDhnQhBBnB
+HJqMqtZbJMmJTqtLtVMqhpfphNdQfhfzzjhhlHll
+rWSBrnwFwWCvwWCwBgPgCgzjQccQhhgRzcdQzjfcNfzR
+CWBCwCvCvvwssWLMtJJGMdMZJsGV
+nFwSFQwsNrrsssSwCrhrCNnfcCRgJRMJTJcMfRzMCMCRvW
+DdbGdLZLttllWWvTzgzzgR
+ZqGzPdLtDjBjDZGPZVmnhQFwqrFQhVFnss
+sNNpCjttjsJjSpgpWjslCTnqqSVffrnhSfDhmhrhfm
+dBwcGzbPBHbbwZcwJbcTTFDFFFDVrdVmFdnDqf
+HzGcczQPHGwzPzGHRctWlvRgtvJlvNlJvRNj
+cFNCFdvcCHvFBCZcwBfRSpttGhDmCghGShmSRt
+QjLnTTzQVzTTnLMqhDgPhGDDSjGPrgSh
+TTJGnJJlLQdNWZWJNBJZ
+WHBpHcMDZHLDbHLtGCnmRmLNGmRqvsCC
+PzTFzPPTJzrSbGsvnmqfsqfqRz
+dJSQQdVFQgjTrjQPWcWHbBVcZVccZtWp
+JDtnRtJzNzTTNlHc
+rQPJFrLPGMMwrGPFwjFMVLjSTWHdWBTdSWdWZlcWTHlZ
+MGFrCvLLwrwPFVVhvLMGGtnqfsmRJgDnqbRgfbqmfC
+jnTtFjcSSvctJjznzvFmpqqPMqQDRVpRqPzqQzVQ
+bhHBfrWpfHsfGNllRrRCPqDCqPqq
+gsGZZpbWgbwHWGNgfZNjvLSTTjtnTgjSSSSJmL
+RLQNdVNnRQdQHVVLGpspNqvtsqptqpTtsp
+MlRWwbRBBFMFjCTFTTFDvj
+WmlWBmBwwmrndnmLRHRQ
+WnftJWlfnWSHGCjWWWSCFqFGBDqBwMcDmmMmGmqD
+pTNhpTrPhhhRPzbhrppLhThLgqDmwccwqPBmMMqnFBcwwmBB
+ZrhQTpzdjSVVtnvZ
+jgtnJtBjtlTdJBZJVQBngQGDCGWpPGCcPWCbWdWMbcpP
+wHstNNttSHPDmHcMCp
+rrFFSvLLNfsFtNSqrtfrhsNjjVTBVgVvnTBvTQvjTgjVZz
+qhZwlqFqFwlJwrDHqHcDvgcNzv
+RCCTQmjCbQTBtRTbjJRDpBrgDHPPpPDvHccDPc
+VmjVWstQJhlJlGVJ
+GggpGwZmgvgJMvbJFQQDbDFbBbFCQDCW
+rtrLzNLtNSPnNqDSQDcQCWlqBQ
+VtzdRPtztLtVRtZmmMTRwCGZpMwp
+vtvqjsCqtshfjcWFHWGjGFJj
+zGrnzDDMpPcTHcSTVTJP
+DbDwMbZRDrZdBBnMznZMGZDfwtlgQhsqCttClsqvsLfCff
+JLzLtLsrzsQdvrWRwMHwcc
+qPmCTzlPjljjFTZmWwcwwvHMMRWwHvMm
+PFqZnVCqTCNjCzNgQsbGBLzLQQ
+CBnppDHllVpPCBshBHpjDTSmZcSrfwvmJcDDTJfw
+dFRLdLFQzNSTBTSNmBJv
+FzFFzRMBFWtQlPlsjjPVMnhC
+CVCfwnfdVvBdBbTNTT
+LNzsHPNWsDjTZqDHqT
+PtLgQsGQLSzWLstPgGWcgQLSNrpplffrnrNhpVCwlGVlrwMn
+jPPVqPsHffzVnHzvSgMcCJGGMSVCll
+pdbpDpBLNmNNppJgcvgSllGjDSGQ
+hrbBwLpjLhhhNZLhNrhZZLHzfsztFzzsrtHfRFnFfRHf
+tdjBdbmSfdHBdHHmZlWjFrnlWQlqvMFvFn
+pDNDJhLhPVPLLLJphJLwNcwnQTcWWTqTrqMWTZvqMrlvFM
+gpVLhNwpgZJCghCLDNwphgmsBdzHHHmSstmfggzdbR
+TfMpfMBVftLMDBSjWDHgzHbgwLgHHvdzggzs
+QJnZcFFnZRHdHjJvwgdg
+RjjRRnmNmmZNjZqZnQcVffBrWqVTqrtffTSTVV
+fZTdTVcVjrjdBzdTnGtgnnGSHHNFGn
+thMWPtPMslmGnWnNnS
+thvbMvQMRphhLCjrzBjZVdcQfC
+MpmgZFgMGdrFrBCVnJ
+JsbJlTTlvLQbVffRRvBBRVjd
+LWlbhHlJhLTJmmGcMMHNmNgN
+bhvmhPrbhqNqQRRGzQjVvvRL
+wTwBZDBTwwggfnngcDfdsVVFQCdzCzDVRsFdQs
+pngWMcgzMgpZWncnMpWNrbNHrNbmHhltWlbl
+nPndBjLPscWSccBVGnScsSzMdhMppMthdMgpMgrzvhhp
+CCFTFDwqZqCCJmhvpDzztVzDNztp
+qQFJTbRVbmCfwTwfmnnssWBGnLnWlRLSGn
+JRlJDSvLRRCdvmDSvdlbZNVBSWZGNgWsZGNgZBVs
+QrjPMqMnLzzjLjFnNNgBpsgtgGGGVZ
+hrjrFqjqFrQfMHPhQzDvvCLJdwwwmvbJbwDH
+HDGrDDDpNsGQNdZQ
+jpjgtgjSjpjllfZZtZsvNdtshqqq
+cbgMfjclWTJcMwjWJfpfmVPLPBnVBHnmVbnmLBbD
+rPrMZNsNrsvrwqvFFFdgQWNzLJJzRW
+pStppStHmcmHpgVSllVcbVbWWDdLFhdbzdRRFhJFLLRF
+cltCHmCBmtSlgjpllgGvTwPZPMfZvPsCMCwZvC
+FRQQMdlFMDWRFQRQMQQDWdFbSSSVJSBbJSlBVVBnPJnzJL
+rsftPfhsrgwznSzzHSLgJG
+fhNsjrjhvsTTvdjcCRMRMRPcCW
+tRtJttHFrjtDQHHBQMMBgMBSghhZQb
+vqWPLpLvqrmPdmqwvqfmPhNBBBlSnbwbgnlnlhNSZZ
+pGpdfzLLspddmqsqPvfvvPpGTVcJJCDRjHrccRtDjcRDFD
+GJMHCdTMWJRhSTlhhSPllt
+fVvqpfBFrqvqNzzgVDFrpDPmSVtQSlSmhjwltlRtmVhn
+pzpBNDBzfDrsNsDRJJRdCssMLdLZWZ
+hFfvWWvdpCwwcwFhphpcZCMmllHLfmbQlbrQLBJmGgQrQm
+nVSNGjGzzSVNTsjzSJrbSrHBSHlrrmQHJB
+PttTNsTRVnNjNqRnzRzRWCCCcpMCWGPMwwFZvFwW
+DvZbFnDDsqDBwwRQgNBm
+HhWpWWRMWChlChdHLlGlGtQtggSNPSSpgPmNgPJQtw
+CMlWGMhhCVHlLCdHTHdrGHdbzjVqnzcvqqjRjFbbzbFFqR
+ZZgCNqqBmjZsNgZCqJgNBdrLFHbBrWlPdHWFbPnHPW
+TVwTDfzDwSDzmcSTcrzdbllnHPHdFlLzbF
+tvDQwVtVvDVmtRsNMgRpJg
+BBpDCpNJnmnpnDDmDGGmtTzqHcGTvTTjTbGjHLVcLb
+swNNhPwwHzTVwwHw
+rPRlPRhSQmmBDpnNfl
+pbRhffPzcPDmfcNTpVBLpBjMGBGjZLLg
+ssrCsqrszgJjZMqZLQ
+SzCwnsllCrssvdrvwzPmDmPPbFRbSThPTPDD
+QWLfcfczQQpcDTpLPfdZRRvRRVqbFWvZbvtqvv
+NsGGJBhCmNdZVqsbdssZ
+rMwwwBBJMrdzPfQMpzMnLQ
+rdtCQhrCtQQprtTWQCHFjPgGBPdFPgvBqRRPqB
+lsVsSnVSbLmmgBcgTLTFGgvq
+wTTDTszsbzMDppJrJhDQ
+ZlmsGLBVCBBZFCFFHqcHVvQhqVQSSHpH
+dbbTRMrRwwzDfrTbFtMvcptvHFQQpqtc
+gdJTDWgfwDwTwmgmNPnNsgFBlZ
+PWhWhGFzzzrLdHCPccbJQJcHPD
+NRpVTpTgRWVlHJNHMcHQMb
+pSRSpVSZWRSTZjgTRTWnFLdZLrrndhdzZvtvzn
+LgctLgVBVLhlPjqRhBLVcVlhbDDcGnNGfwCrbNDnrGbGCJNw
+HmppHMWWmQmMqZZHWQrDDfDCffJrDGJrCb
+qpWZsZZZMZWTLPhTRgTtThRP
+hfhQfFQWzBfhfTQdmzdLDtDjtvHLjt
+qsgpcqMNRmgpqsCwpCmZDvjwvdddHZHvZDtrrd
+SgmNmqScbTSJbhJQ
+dvMTQvTnZJsrQdbbSvMVZMblDwlflfDGgwwHcfGjPfjjrG
+FqBLpBpFpFzRzqNFmgjGlDRHcwPPGwgcgs
+tLNtWshsLLqWMJhTQVVbhvJd
+bgZLMZgzbbLCcPCbMZbcNMgBqSTqSWVtSzvvTTBTqBvRBW
+FhQpJQnGrlhGlrnTqRtTRJqSwDwtVR
+FqlnnFnqFHnGHHdNZdbZbCNMdPLMPb
+HHFnbftcfnfbbTbTnHTNVZZzJlPQlFrFzVJFZsdr
+mvpGCBgwqCvLCqvMQWWzsQQWlPzwzsrV
+hpGSGgqSvqbHcnhfVfct
+lGVrnHsGcnVHzscrlGjHcrHqqWPlJCPJClTLLqCSPLPdqS
+fRbwbtMQZtMMRFMSqfJTTWCTJPJCmd
+ggdvtvdbVVGnpDGg
+BnBjTcbnvhjjlMnNJJfnDnQDGdNDfP
+qwFqVSWwqLpWFmFVCSqFpDDCJNJRQTRfRDGPfdfDQN
+zwHwWVVWWFSqqwWTLqzzztHMvBhlcghblMlcttMllh
+PFFNPNPmlFllbctNLmcjBstrsVrQHJSSHHSnnB
+fddfDhdwGhTWWTDMwMggssjsjndsBsjsnSrVqSVV
+MCvvTWvRMwCvGPpzCcmbplpCVC
+thTqlPPTNbGNhGdqRRhRrNtFWnDnvvFZDpnFvfQDZtvWvv
+HcMzVcVVcHrgHzcMcmmgfQvFQnMjnWDjfnvjQFfQ
+VSmHJLHBJrTrJTlT
+NjnsHjLLjNRddNdBFBSR
+ftsbqfDcDqsrDtqsfSVBhJVFJgdBRVFS
+wvDqwtDlsDDczjzjHvLzLQQM
+qDwstwDtRfpJfVhBVZBMvnlRvv
+zSFzQHFWdgZBVTZhTzrp
+NHdggjGjWHQFPWHNPPbpJfPDtCwCtqDqJfbt
+pvnbqHvnTvlCCpjsBsMGBGWWPp
+RJSJhJCRVJmJwScrhSJdfwFsBGhZBjhGFFFFGgFPGhZW
+cdRrdmwtfcdSmLtcSCQlvQNqQTlqqvtTlv
+rnSlSrgWjVGpTTRhSffpRd
+HtgHPsNNgNHszPcTBphMdhHhBcTc
+JNNbZPZZsszNmtDbPgsmJlwFvWVnCwrlmWlnjnGvCC
+WrVBVgVGGQCrSTTqvVjDqDjv
+FmwRRwwRQhhLFMjFMzdqSSzS
+RcJtbnnLtQWrGHcfrP
+vpzssjmVjVZWNZzzQwtQwccpQhgtQCct
+qDdfLMnMrrTbBLqTqltlTfbGQnghgRwggGgRnhhccCJJcG
+MtdLfSSSddMftlrjjzsSWVSjFvjNvs
+qTRPpRPzJglzGJzpGRHWHljwDtbwffjtbhjfwNfHmwwf
+SZLVdsvrrdFdBcdZvsBdDCNtbmftNwfNbNhCNvtz
+MLzzddLsQRppRlQGPq
+PDDpdJgtpppGgttgdGdgJFzLjVcvVnnCTrVrRPTLvwnTTC
+ZSbHBsSNlZcsfNnnvRrnVjrHwvCC
+NmSmsfsfhmzcDmctJW
+NbrLfrrLqpqWQHtBzbFttJgcgB
+CmwjPPjjjShPvljwvwwjPBFttBtcHzFJcHTRHJRRmT
+CCljjDGhvPCVdVSCdPvrrNfnnQsGqMpqqMqnFW
+bdPdbcDZlddsZbHjrrgrmZmCZhCGjv
+BffLfLVFVMMBRfwMpfzhFGFhGWvWvrhNvvNj
+RMBpRSnffBJjSbJqdPHsDcbqtl
+BgwGwDDZttDDTNND
+WzNNnFRWFtTFlFsh
+WJjPpPqqzWRbrqnNqvVvgvvdcBwgdrVBZG
+FFbMVMFPvJppgvcvrZMjHlCJWHmHHBlqhCmqChCl
+RGQVdVVLnLsQnQnnqWBlBmDRBDWWlhBD
+SftLzQndGfVgFfjvvM
+npvLlFLTWWqdLnJCmBmmpjQjjmjB
+tfgDwzwVVVVtgtrsJtrbjSPQjQmjNBCNBNhPHDHC
+ggVzVtMRgzMrvJLqFnnRnnRT
+gZFZssWgNZTDwHDWzsFwWDQMMpqqpBPMjFtMPSQFqqqM
+vrmvhdnVvQpftStnMN
+JCdLddhhdJdcCdrrmCGhlgNsWDWDwWsgwHgHLZHW
+vSsSGjSPvjvRSGpFprFbqFpppRfp
+ZdmlndtBZbwrwfpWFn
+JmdHdBBHtgllZldBhJZldLLBjPVQTfvGPNzQQSjjzPgTGNTs
+TjTjBjVrTsLRRrMBsMMgzLqGGqgQHQdCQGgpgd
+nbZcmNnPNcbNftvhlhZpgQgCqdSpgCHCqPFzSH
+WfcNvtmmNmQlvNcbsWWjMwMVMRDVMDJM
+hHHnfZSwHDgHcfclSGSnvrnvBCvWWntvzvzbWWVq
+dTJTmspFTsFdRRLvtvLzvvVqtPVtrb
+VFMNFpRJNTppTpsJVRcMGgfGfgZwghgGfcGh
+lLGvwsMJLCMVnTrCrVdHRd
+tbzqtDNNBpNWBtqzfRrFFnrVTTdrQVSVGp
+tztfzmfzzPDzgWNNBbhGMlJLsvhJJjGJhGmM
+rHrVJQVQVJLggDQQLbTvdCCSTdWLLLbCbS
+pNtnwPthmZGRpmPFtqbMSzqffFSdTvbSzW
+GpvvshwtmwsZDljjssHjVBVj
+SmhJdtJhhzQSrzVhtQbtBRNfnFNSnDNGRfFGGMgR
+lHwqPjqwTjLHCWLvPpvNrNMvnNGNfNGNBffGRN
+lCrPTrwpPZWlqPlqpWWqZjsmJzbzVtVhhdsJcQdddZVJ
+QqpCWHdQdVQlWcQCqcfRjnZZZPDnSPqPhhqZ
+tmmzgWGgwJwwStSZZDRnZssR
+GbFbLLvgmMMwGgmLCrppQrWVlWrFTHHd
+qdqCgSVdVSVqfwsdZhpJspZsph
+RjZBbmRlrlmmJwLNNNhLpwhB
+vZlRrtTZCzCMfSPT
+JBjhCNwjrlJlHJJRsscZrTcvLLgTsLPP
+dDztmntCSgbLgqTzgc
+fGVWnSMFtVGMNNNQllBjWHJC
+dSDhVVdVZtnSgHQGThQvFNQQqF
+LcfLRpMpcBpbrJfsbsscBNWRNPRGHvqPdTGPGqPWFH
+spmrCcrdJJpLLmcmLLLDlzSzCCjVwZVtgnSZzn
+ZJtgPTHtZPZQGbtNzzprVWWVrbrpCD
+BRlfcRmBhSMVBqSVfBvNWrDrjWCjjzprCDCl
+LfMRSmfqfSLcnnMqVSfccnhZwJFHZFTGZQGFLggwZTZGPJ
+BChWddRRRcfmDbfhDP
+MgpMFFsvMfGwvLgPjQPzPPmDcztDtw
+NFgJpqvpLgqFnWWVNSfnNSCR
+zMMMRmMfJpfhpzQJLMVtjtjPntgBtlZlVgJP
+SdNbZvZbvbHTNbZbSWTdrTVBglBDlWBjDPDgntPqBDPt
+rrNcrFwNdSrfzwMzZMLQQs
+JPmCSfHTGJdTCbHgpgqLgRhghhffhg
+ZWSSFVSVFQghQvwpphgh
+lsDtZjVMMSdCNdbGCbjb
+PBQPvDvVVRvQDqLDzJTlzwjz
+tGcZTcdgGcncdrFrsTjzJSJqJqqwHSzzSZwq
+CgtgdFgcFCMnMgsGFGGQWPQpQTCBvbNpNVVWVT
+FHVFWMHMgVhnLWWMpnppfcdZNcPplnfn
+RSvSCBSqGgDRjqCpPlPpppTpPjlcNP
+zRzsGgJDqJwLVWVFwM
+ThhWhNthVWTWqbWbFWbTdBtWSdMlHSlGlCGCdsCMClmnSlMn
+DPPpvvfDHfLgDHvzpvPDsssMsmmzsMcClScMMcGG
+rgPLHHJJHgZfvvZQQZfrpfFqBBwFBTNTBNBwtthQwVqt
+JJgSWDSmSDQCFrhbRLSwLS
+VznqzVNsMsZLdqslbRChtbHdHRrwHb
+LVfNLMsLTmWDpBpf
+cbTsnNpcnnchllFQlMRgJhRP
+WddmdMVSBMWSBWjwCJVCPRwPFCQRRC
+DWSSfjdSrTpDnHHMbZ
+fgsVqqwQQtHhCrDfJH
+pvbnBZWBbvWbTdthrJbDmqrmHq
+TvZSNSNNSvFMBpqpnLnTBZBFGQwlQFggcFzcVGRlswsRll
+zfMcQHzPtRNvlllc
+BLnMhbZMLJLNNVtCdNgZgt
+BJGFpqMBhBLLMqnwBhbrbhLssjFzssjfDzsmFmjmQFQPjT
+JPBJPnpBFrqBJHtjlCjHJcCthM
+wQZggQWQGfZFVmmGfDRjlvvNcvlNDhcNttlctt
+SfmWfwVFwVGZWQVGSTdTbBpTPSqBbLnnrn
+RJqBRJbqpqqJGvqHMmcfczfcjvHQfm
+llgVnSWSlsssTnlWjhTcsZMcZcZMZMcccMNmcH
+TnFhhllnWCnVCTllLnhhVSrbDDPrdpjRRqjBPRBBpbFJ
+mRwRRNDjNTqwDNjNnNRTsQLcQWpQWZJLlLpQWs
+PMFGCSzzgbBVzCGShVQZcgWsQLLftttQtlZZ
+SlGVldCGGbBPMhPCVSBrNNdjRqjDrNmDnmrRwN
+mqGGqGHnqGBCMrnGCbbbLgTTFFNNghHNTj
+SdRfcsDPPcDdRzWPWltSlscwTSbShhgpQhgbFLbTQFjwhN
+DPWWZzzztsDDtfzlscsPdcWZnVMNVqqGJnBrVqCrMrVvZnBJ
+ZgglFCrrrlrWCJswHmwRVmFSwSsP
+zhzqBLcjjnpzMzjhTtcqnVGbwssVRmqbHNPbwPsVNH
+ftBjzLtptRWdvZlQQZQf
+nGpsMncVRMGSnfsBllZdppwrTljZrQ
+gcgHmtbCthHWhwBFBWZBlWlWrd
+bCDDqHhcqbbtqcqtvJMzsGRsvVfPsfnzJV
+TclPvSGZsPZRjhjWDgjp
+JtnwHFtJqtwfQfgWgRWhdhjtgdRM
+JBwnHwgFFqVJrsGmPvNTPsVvSN
+ZJnfZNnDNZJLzNntDtDNNzNWTVBPrrvRRGdBcVRfPPcvfdMr
+CFgjFmggQSQQmSggVMMvRdTvBVRjrdrc
+mbsqQFqFgwwmgmSbwQWWLDWzpLcLnzZzLbLL
+PnwSFSLSTwbbHdtstW
+RrDZVVfJNZCmDCfVDVlblZHbddtHScbWbMjt
+NmzqhzCCqmzffhCCqrhhLLnPvpnTPPgpGTTBSL
+ShhfLSDDFMPQddpMrDgNbjzffqqqzgcjbqZR
+sCstmwJwVBtmTltVmTVbRbcbcRvqvrZvBRvZbR
+VwCnwnVrrrWShWPHHDdQFL
+pbpDpWjZMmFCmmmb
+jTjtJLJgJncCFmnJFC
+LvhvhTQhBSdRNtLNsSszlGrHSGjZDlGf
+JrhvTNJJhhCrtVtcrNLwDBSBwqzDwQVbBLQS
+RnCgHmHHGMdPsGMfDlDqlSQbQnQQDbzD
+RdPMPsmWHmjfMffPcCWrptcprpFTFrFp`;
+
+// part 1
+const rucksack_compartments_checker = (rucksack) => {
+  const compartment_1 = rucksack.slice(0, rucksack.length / 2).split("");
+  const compartment_2 = rucksack.slice(rucksack.length / 2).split("");
+
+  return compartment_1.find((l1) => compartment_2.find((l2) => l1 == l2));
+};
+
+console.assert(
+  rucksack_compartments_checker("vJrwpWtwJgWrhcsFMMfFFhFp") == "p",
+  "The first rucksack contains the items `vJrwpWtwJgWrhcsFMMfFFhFp`, which means its first compartment contains the items `vJrwpWtwJgWr`, while the second compartment contains the items `hcsFMMfFFhFp`. The only item type that appears in both compartments is lowercase p."
+);
+console.assert(
+  rucksack_compartments_checker("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL") == "L",
+  "The second rucksack's compartments contain `jqHRNqRjqzjGDLGL` and `rsFMfFZSrLrFZsSL`. The only item type that appears in both compartments is uppercase L."
+);
+console.assert(
+  rucksack_compartments_checker("PmmdzqPrVvPwwTWBwg") == "P",
+  "The third rucksack's compartments contain `PmmdzqPrV` and `vPwwTWBwg`; the only common item type is uppercase P."
+);
+console.assert(
+  rucksack_compartments_checker("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn") == "v",
+  "The fourth rucksack's compartments only share item type v."
+);
+console.assert(
+  rucksack_compartments_checker("ttgJtRGJQctTZtZT") == "t",
+  "The fifth rucksack's compartments only share item type t."
+);
+console.assert(
+  rucksack_compartments_checker("CrZsJsPPZsGzwwsLwLmpwMDw") == "s",
+  "The sixth rucksack's compartments only share item type s."
+);
+
+const letter_score = (letter) => {
+  if (!letter) console.error("Bad argument", { letter });
+
+  if (letter.charCodeAt() > 96) return letter.charCodeAt() - 96;
+  return letter.charCodeAt() - 64 + 26;
+};
+
+console.assert(
+  letter_score("a") == 1,
+  "Lowercase item types a through z have priorities 1 through 26."
+);
+console.assert(
+  letter_score("z") == 26,
+  "Lowercase item types a through z have priorities 1 through 26."
+);
+console.assert(
+  letter_score("A") == 27,
+  "Uppercase item types A through Z have priorities 27 through 52."
+);
+console.assert(
+  letter_score("Z") == 52,
+  "Uppercase item types A through Z have priorities 27 through 52."
+);
+
+const test_case_compartments = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`;
+
+console.assert(
+  test_case_compartments
+    .split("\n")
+    .map(rucksack_compartments_checker)
+    .map(letter_score)
+    .reduce((a, l) => +a + +l, 0) == 157,
+  "In the above example, the priority of the item type that appears in both compartments of each rucksack is 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157."
+);
+
+// solution for 1st part
+console.table({
+  "result 1": rucksacks
+    .split("\n")
+    .map(rucksack_compartments_checker)
+    .map(letter_score)
+    .reduce((a, l) => +a + +l, 0),
+});
+
+// part 2
+
+const group_checker = (group) => {
+  const [first, second, third] = group.split("\n");
+
+  if (!first || !second || !third)
+    return console.error("Bad argument", { group });
+
+  for (const l of first) {
+    const second_without_letter = second.replace(l, "");
+    const third_without_letter = third.replace(l, "");
+
+    if (second_without_letter.length < second.length)
+      if (third_without_letter.length < third.length) return l;
+  }
+};
+
+const test_case_group_1 = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg`;
+const test_case_group_2 = `wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`;
+
+console.assert(
+  group_checker(test_case_group_1) == "r",
+  "In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges."
+);
+console.assert(
+  group_checker(test_case_group_2) == "Z",
+  "In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges."
+);
+
+// solution for 2nd part
+console.table({
+  "result 2": rucksacks
+    .match(/\w+\n\w+\n\w+/g)
+    .map(group_checker)
+    .map(letter_score)
+    .reduce((a, l) => +a + +l, 0),
+});
